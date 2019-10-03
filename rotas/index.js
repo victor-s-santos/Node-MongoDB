aebc79b5fbad531658715fef18b50304a8759437
victor-s-santos <victor.s.silva@unesp.br>
2019-10-03 13:15:22 -0300

    implementando metodo get

--- rotas/index.js
+++ rotas/index.js
@@ -4,9 +4,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.send(`Olá ${req.query.nome}, como vai você?`)
+    // let nome = req.query.nome;
+    // let sobrenome = req.query.sobrenome;
+    // res.json({
+    //     nomecompleto: + nome + " " + sobrenome
+    // })
+    //res.send(`Olá ${req.query.nome}, como vai você?`)
+    res.json(req.query);
 
 });//rota inicial
+
+router.get('/posts/:id', (req, res)=>{
+    let id = req.params.id;
+    res.send(`id do post enviado: ${id}`);
+});
+
 router.get('/sobre', (req, res)=>{
     res.send('Nova página');
 })
