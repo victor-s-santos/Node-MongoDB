b948fddf72f439f3338ce1be1cb7c98231373cbb
victor-s-santos <victor.s.silva@unesp.br>
2019-10-03 12:23:44 -0300

    nova rota adicionada

--- rotas/index.js
+++ rotas/index.js
@@ -2,9 +2,14 @@
 const express = require('express');
 
 const router = express.Router();
+
 router.get('/', (req, res)=>{
     res.send('Olá Estranho!')
 
 });//rota inicial
+router.get('/sobre', (req, res)=>{
+    res.send('Nova página');
+})
+
 
 module.exports = router;
