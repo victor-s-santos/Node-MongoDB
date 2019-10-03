b5f13d782300e6d82161e022fe3a6318906db7aa
victor-s-santos <victor.s.silva@unesp.br>
2019-10-03 11:32:27 -0300

    usando as variaveis de ambiente

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 
-app.set('port', 3000);
+require('dotenv').config({path:'variables.env'});
+
+app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), ()=>{
     console.log(`Servidor inicializado com sucesso na porta ${server.address().port}`)
 });
