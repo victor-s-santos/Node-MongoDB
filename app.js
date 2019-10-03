9c8756f5223b2df253f37d64cfbfe3cb451a1bee
victor-s-santos <victor.s.silva@unesp.br>
2019-10-03 13:31:10 -0300

    mustache instalado e configurado

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express');
+const mustache = require('mustache-express');
 const router = require('./rotas/index');//chamando minhas rotas externamente
-//definindo rotas
-// const router = express.Router();
-// router.get('/', (req, res)=>{
-//     res.send('Olá Estranho!')
-
-// });
-
-//fim das rotas
 
 const app = express();//inicializando o servidor
 app.use('/', router);
 //app.use('/admin', adminRouter);
 
+app.use(express.json()); //não há mais necessidade de body-parser
 module.exports = app;
