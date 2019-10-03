282b67f7b6881c198c6b8c6990a67fc85e26b775
victor-s-santos <victor.s.silva@unesp.br>
2019-10-03 12:16:23 -0300

    esquematizando a estrutura das rotas

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 //definindo rotas
+
 const router = express.Router();
 router.get('/', (req, res)=>{
     res.send('Olá Estranho!')
 
 });//rota inicial
 
+//fim das rotas
 
 const app = express();//inicializando o servidor
 app.use('/', router);
+//app.use('/admin', adminRouter);
 
 module.exports = app;
