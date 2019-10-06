cbf9429ecb2691a6147f6903298395046f1bc259
victor-s-santos <victor.s.silva@unesp.br>
2019-10-06 16:25:10 -0300

    implementando o padrao mvc

--- helpers.js
+++ helpers.js
@@ -2,5 +2,6 @@ exports.defaultTituloPagina = "Título da Página Padrão";
 exports.menu = [
     {name:'Inicial', slug:'/'},
     {name:'Sobre', slug:'/sobre'},
-    {name:'Trabalhe Conosco', slug:'/trabalhe_conosco'}
+    {name:'Trabalhe Conosco', slug:'/trabalhe_conosco'},
+    {name:'Login', slug:'/users/login'}
 ];
--- rotas/index.js
+++ rotas/index.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
 const homeController = require('../controllers/homeController');
+const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', homeController.index)
+//router.get('rota', controller);
+router.get('/users/login', userController.login);
+
 // router.get('/', (req, res)=>{
 //     let itens = {
 //         'nome': req.query.nome,
