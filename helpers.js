45c56052f9eb26e26940cab46d04fb87a2a5a0b0
victor-s-santos <victor.s.silva@unesp.br>
2019-10-06 03:15:32 -0300

    terminada a implementacao de template helpers na criacao do sistema de menus

--- helpers.js
+++ helpers.js
@@ -1,1 +1,6 @@
 exports.defaultTituloPagina = "Título da Página Padrão";
+exports.menu = [
+    {name:'Inicial', slug:'/'},
+    {name:'Sobre', slug:'/sobre'},
+    {name:'Trabalhe Conosco', slug:'/trabalhe_conosco'}
+];
