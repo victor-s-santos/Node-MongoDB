75bb72ee567243a58789fbe9c45e3c27e9526c3b
victor-s-santos <victor.s.silva@unesp.br>
2019-10-07 12:45:40 -0300

    definindo msg flash: habilitando cookie e sessao

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mustache = require('mustache-express');
+//flash message
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('express-flash');
+//
 const router = require('./rotas/index');//chamando minhas rotas externamente
 const helpers = require('./helpers');
 const errorHandler = require('./handlers/errorHandler')
@@ -12,6 +17,14 @@ app.use((req, res, next)=>{
 });
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+//habilitando cookie
+app.use(cookieParser(process.env.SECRET));
+//habilitando sessao
+app.use(session({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUnitialized:false
+}));
 
 app.use('/', router);
  
