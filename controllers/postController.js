a86a9097052fd0e574d32bd0cc1745f41cbb8b19
victor-s-santos <victor.s.silva@unesp.br>
2019-10-07 12:07:43 -0300

    usando async await para resolver a requisicao assincrona

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,10 @@ exports.add = (req, res) =>{
 };
 
 exports.addAction = (req, res) => {
-    res.json( req.body );
+    //res.json( req.body );
+    const post = new Post(req.body);
+    post.save();
+    req.redirect('/');
 };
 //requisicao get -> recebe como query
 //requisicao post -> recebe como o corpo da requisicao pelo body
