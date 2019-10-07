d12645591594f02a1699f44148b754273dea8c82
victor-s-santos <victor.s.silva@unesp.br>
2019-10-07 18:27:06 -0300

    implementado o sistema de listagem dos posts

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,32 +1,13 @@
-/*
-exports.userMiddleware = (req, res, next) => {
-    let info = {name:'Victor', id:01};
-    req.userInfo = info;
-    next();
-};
-*/
-exports.index = (req, res)=>{
-    /*
-    let itens = {
-        'nome': req.query.nome,
-        'idade': req.query.idade,
-        'mostrar': true,
-        userInfo: req.userInfo,
-        lp:[
-            {linguagem:'python', framework:'django'},
-            {linguagem:'javascript', framework:'vuejs'},
-            {linguagem:'php', framework:'Laravel'},
-            {linguagem:'javascript', framework:'react'},
-            {linguagem:'ruby', framework:'on rails'},
-            {linguagem:'python', framework:'flask'}
-        ],
-        aprender:['machine learning', 'deep learning', 
-                'tensorflow', 'keras',
-                'pytorch', 'pandas'],
-        hello:'<h1>Olá estranho!</h1>',
-        titulo: 'Título da Página^^'
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
 
+exports.index = async (req, res)=>{
+    let response = {
+        titulo:'Bem vindo(a)',
+        posts:[]
     };
-    */
-    res.render('home');
+    const posts = await Post.find();
+    response.posts = posts;
+    
+    res.render('home', response);
 }
