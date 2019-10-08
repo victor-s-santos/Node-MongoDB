f5ae912d81c238a08f28dda7fb7fc26ebd682c38
victor-s-santos <victor.s.silva@unesp.br>
2019-10-08 17:16:49 -0300

    encaixado o css na pagina, mas ainda precisa de alguns reparos quanto ao layout

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ const app = express();//inicializando o servidor
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+//tornando public um diretorio estatico
+app.use(express.static(__dirname + '/public'));
+
 //habilitando cookie
 app.use(cookieParser(process.env.SECRET));
+
 //habilitando sessao
 app.use(session({
     secret:process.env.SECRET,
@@ -30,13 +35,11 @@ app.use((req, res, next)=>{
     next();
 });
 
+
 app.use('/', router);
  
 
 app.use(errorHandler.notFound)
-// app.use((req, res, next) => {
-//     res.send('O endereço que você informou não foi encontrado!')
-// })
 
 app.engine('mst', mustache(__dirname+'/views/partials', '.mst'));
 app.set('view engine', 'mst');
