4329532350cdd87d270a3cbf7b3475e6d4c079a7
victor-s-santos <victor.s.silva@unesp.br>
2019-10-08 11:42:34 -0300

    lower case definido true

--- models/Post.js
+++ models/Post.js
@@ -20,7 +20,7 @@ const postSchema = new mongoose.Schema({
 
 postSchema.pre('save', function(next){
     if(this.isModified('title')){
-        this.slug = slug(this.title);
+        this.slug = slug(this.title, {lower:true});
     }
     next();
 });
